19bdb63e1f99b55d98e6a5c6d333ad6b797d26bc
Kevin Ashley <kashl007@odu.edu>
2017-02-15 21:24:26 -0500

    Media query tweak

--- assets/slidesjs/source/jquery.slides.js
+++ assets/slidesjs/source/jquery.slides.js
@@ -129,12 +129,12 @@
       }
       $(".slidesjs-next", $element).click(function(e) {
         e.preventDefault();
-        _this.stop(true);
+        //_this.stop(false);
         return _this.next(_this.options.navigation.effect);
       });
       $(".slidesjs-previous", $element).click(function(e) {
         e.preventDefault();
-        _this.stop(true);
+        //_this.stop(false);
         return _this.previous(_this.options.navigation.effect);
       });
       if (this.options.play.active) {
@@ -180,7 +180,7 @@
           }).appendTo(paginationItem);
           return paginationLink.click(function(e) {
             e.preventDefault();
-            _this.stop(true);
+            //_this.stop(true);
             return _this.goto(($(e.currentTarget).attr("data-slidesjs-item") * 1) + 1);
           });
         });
